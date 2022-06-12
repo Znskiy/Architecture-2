4d85403832f9ee71d775b146d82fad80e58e8701
Vitaliy Yanyshyn <alkatel-3@ukr.net>
2022-06-12 19:22:52 +0300

    Update Handler_test.go

--- Handler_test.go
+++ Handler_test.go
@@ -1,63 +1,63 @@
 package lab2
 
- import (
- 	"flag"
- 	"fmt"
-	"io"
- 	"os"
- 	"strings"
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"io"
+// 	"os"
+// 	"strings"
 
- 	lab2 "github.com/Da6hkin/Architecture-2"
- )
+// 	lab2 "github.com/Da6hkin/Architecture-2"
+// )
 
- var (
- 	inputExpression     = flag.String("e", "", "Expression to convert")
- 	inputFileExpression = flag.String("fe", "", "File with an expression to convert")
- 	outputFile          = flag.String("o", "", "File for the converted expression")
- )
- var (
- 	reader io.Reader
- 	writer io.Writer
- 	err    error
- )
+// var (
+// 	inputExpression     = flag.String("e", "", "Expression to convert")
+// 	inputFileExpression = flag.String("fe", "", "File with an expression to convert")
+// 	outputFile          = flag.String("o", "", "File for the converted expression")
+// )
+// var (
+// 	reader io.Reader
+// 	writer io.Writer
+// 	err    error
+// )
 
- func main() {
- 	flag.Parse()
- 	if *inputExpression != "" && *inputFileExpression != "" {
- 		fmt.Fprintln(os.Stderr, "There should be 1 input")
- 		os.Exit(1)
- 	} else if *inputExpression != "" {
- 		reader = strings.NewReader(*inputExpression)
- 	} else if *inputFileExpression != "" {
- 		reader = strings.NewReader(*inputFileExpression)
- 		reader, err = os.Open(*inputFileExpression)
- 		if err != nil {
- 			fmt.Fprintln(os.Stderr, "Filepath is incorrect")
- 			os.Exit(1)
- 		}
- 	} else {
- 		fmt.Fprintln(os.Stderr, "There should be 1 input")
- 		os.Exit(1)
- 	}
-	if *outputFile != "" {
- 		writer, err = os.Create(*outputFile)
- 		if err != nil {
- 			fmt.Fprintln(os.Stderr, err)
- 			os.Exit(1)
- 		}
- 	} else {
- 		writer = os.Stdout
- 	}
+// func main() {
+// 	flag.Parse()
+// 	if *inputExpression != "" && *inputFileExpression != "" {
+// 		fmt.Fprintln(os.Stderr, "There should be 1 input")
+// 		os.Exit(1)
+// 	} else if *inputExpression != "" {
+// 		reader = strings.NewReader(*inputExpression)
+// 	} else if *inputFileExpression != "" {
+// 		reader = strings.NewReader(*inputFileExpression)
+// 		reader, err = os.Open(*inputFileExpression)
+// 		if err != nil {
+// 			fmt.Fprintln(os.Stderr, "Filepath is incorrect")
+// 			os.Exit(1)
+// 		}
+// 	} else {
+// 		fmt.Fprintln(os.Stderr, "There should be 1 input")
+// 		os.Exit(1)
+// 	}
+// 	if *outputFile != "" {
+// 		writer, err = os.Create(*outputFile)
+// 		if err != nil {
+// 			fmt.Fprintln(os.Stderr, err)
+// 			os.Exit(1)
+// 		}
+// 	} else {
+// 		writer = os.Stdout
+// 	}
 
- 	handler := &lab2.ComputeHandler{
- 		Input:  reader,
- 		Output: writer,
- 	}
- 	err := handler.Compute()
- 	if err != nil {
- 		fmt.Fprintln(os.Stderr, err)
- 		os.Exit(1)
- 	}
- 	res, _ := lab2.prefixToInfix("+ 2 2")
- 	fmt.Println(res)
- }
+// 	handler := &lab2.ComputeHandler{
+// 		Input:  reader,
+// 		Output: writer,
+// 	}
+// 	err := handler.Compute()
+// 	if err != nil {
+// 		fmt.Fprintln(os.Stderr, err)
+// 		os.Exit(1)
+// 	}
+// 	res, _ := lab2.prefixToInfix("+ 2 2")
+// 	fmt.Println(res)
+// }
