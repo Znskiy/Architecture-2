ded7cfc6a2e8e4facc6a68b134ce6a0641c516c6
shiwusa <lekh0t0er@gmail.com>
2022-06-22 17:38:25 +0300

    replaced wrong file with tests

--- Handler_test.go
+++ Handler_test.go
@@ -1,63 +1,64 @@
 package lab2
 
-// import (
-// 	"flag"
-// 	"fmt"
-// 	"io"
-// 	"os"
-// 	"strings"
+import (
+	"bytes"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+)
 
-// 	lab2 "github.com/Da6hkin/Architecture-2"
-// )
+func TestConsoleCorrectInput(t *testing.T) {
+	stdout := os.Stdout
+	read, write, _ := os.Pipe()
+	os.Stdout = write
+	handler := ComputeHandler{
+		Input:  bytes.NewBufferString("2 2 +"),
+		Output: write,
+	}
+	err := handler.Compute()
+	write.Close()
+	out, _ := ioutil.ReadAll(read)
+	os.Stdout = stdout
+	if assert.Nil(t, err) {
+		assert.Equal(t, "2 + 2\n", string(out[:]))
+	} else {
+		t.Errorf("Incorrect result")
+	}
+}
 
-// var (
-// 	inputExpression     = flag.String("e", "", "Expression to convert")
-// 	inputFileExpression = flag.String("fe", "", "File with an expression to convert")
-// 	outputFile          = flag.String("o", "", "File for the converted expression")
-// )
-// var (
-// 	reader io.Reader
-// 	writer io.Writer
-// 	err    error
-// )
+func TestConsoleRandomChars(t *testing.T) {
+	stdout := os.Stdout
+	_, write, _ := os.Pipe()
+	os.Stdout = write
+	handler := ComputeHandler{
+		Input:  bytes.NewBufferString("Banana guchi"),
+		Output: write,
+	}
+	err := handler.Compute()
+	write.Close()
+	os.Stdout = stdout
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "Convertation Error", err.Error())
+	} else {
+		t.Errorf("Incorrect result")
+	}
+}
 
-// func main() {
-// 	flag.Parse()
-// 	if *inputExpression != "" && *inputFileExpression != "" {
-// 		fmt.Fprintln(os.Stderr, "There should be 1 input")
-// 		os.Exit(1)
-// 	} else if *inputExpression != "" {
-// 		reader = strings.NewReader(*inputExpression)
-// 	} else if *inputFileExpression != "" {
-// 		reader = strings.NewReader(*inputFileExpression)
-// 		reader, err = os.Open(*inputFileExpression)
-// 		if err != nil {
-// 			fmt.Fprintln(os.Stderr, "Filepath is incorrect")
-// 			os.Exit(1)
-// 		}
-// 	} else {
-// 		fmt.Fprintln(os.Stderr, "There should be 1 input")
-// 		os.Exit(1)
-// 	}
-// 	if *outputFile != "" {
-// 		writer, err = os.Create(*outputFile)
-// 		if err != nil {
-// 			fmt.Fprintln(os.Stderr, err)
-// 			os.Exit(1)
-// 		}
-// 	} else {
-// 		writer = os.Stdout
-// 	}
-
-// 	handler := &lab2.ComputeHandler{
-// 		Input:  reader,
-// 		Output: writer,
-// 	}
-// 	err := handler.Compute()
-// 	if err != nil {
-// 		fmt.Fprintln(os.Stderr, err)
-// 		os.Exit(1)
-// 	}
-// 	res, _ := lab2.prefixToInfix("+ 2 2")
-// 	fmt.Println(res)
-// }
+func TestConsoleOnlyNumbers(t *testing.T) {
+	stdout := os.Stdout
+	_, write, _ := os.Pipe()
+	os.Stdout = write
+	handler := ComputeHandler{
+		Input:  bytes.NewBufferString("3 5 "),
+		Output: write,
+	}
+	err := handler.Compute()
+	write.Close()
+	os.Stdout = stdout
+	if assert.NotNil(t, err) {
+		assert.Equal(t, "Convertation Error", err.Error())
+	} else {
+		t.Errorf("Incorrect result")
+	}
+}
